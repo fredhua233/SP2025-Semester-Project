8fe6f46188da4648455ef564762d4d292397bebb
Michelle <michelle.z@wustl.edu>
2025-02-25 17:50:43 -0600

    update

---
+++ RoboCallerMover/Profile.swift
@@ -0,0 +1,31 @@
+//
+//  Profile.swift
+//  RoboCallerMover
+//
+//  Created by Michelle Zheng  on 2/25/25.
+//
+
+
+struct Profile: Decodable {
+  let username: String?
+  let fullName: String?
+  let website: String?
+
+  enum CodingKeys: String, CodingKey {
+    case username
+    case fullName = "full_name"
+    case website
+  }
+}
+
+struct UpdateProfileParams: Encodable {
+  let username: String
+  let fullName: String
+  let website: String
+
+  enum CodingKeys: String, CodingKey {
+    case username
+    case fullName = "full_name"
+    case website
+  }
+}
