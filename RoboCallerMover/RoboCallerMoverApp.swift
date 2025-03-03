7564b9ce5323babe1ac00516a3d0ea86d9ad156a
Michelle <michelle.z@wustl.edu>
2025-03-03 16:11:18 -0600

    update

--- RoboCallerMover/RoboCallerMoverApp.swift
+++ RoboCallerMover/RoboCallerMoverApp.swift
@@ -8,21 +8,16 @@
 import SwiftUI
 import Supabase
 
-//
-//  RoboCallerMoverApp.swift
-//  RoboCallerMover
-//
-//  Created by Michelle Zheng  on 2/4/25.
-//
-
-import SwiftUI
-import Supabase
-
 @main
 struct RoboCallerMoverApp: App {
+    @State private var session: Session? = nil
     var body: some Scene {
         WindowGroup {
-            LoginScreen() // Always launch at login screen
+            if session == nil {
+                LoginScreen(session: $session)
+            } else {
+                RootTabView(session: $session)
+            }
         }
     }
 }
